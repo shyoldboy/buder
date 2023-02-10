a926b18ee5cfa58c2aa05b24e71a1fa78c3df273
matao <klmatao2008@gmail.com>
2023-02-10 21:01:01 +0800

    Buder project is init!

---
+++ app/src/main/java/com/matao/buder/MainActivity.java
@@ -0,0 +1,20 @@
+package com.matao.buder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+}
